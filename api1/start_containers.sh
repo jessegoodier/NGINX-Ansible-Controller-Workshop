fb00c99473b1a42c69468ae237a0f732402ff323
jessegoodier <jessegoodier@gmail.com>
2020-02-29 10:56:51 -0500

    adding files

---
+++ api1/start_containers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Removing old F1 API containers"
+docker container rm -f workshop-f1-api workshop-db
+echo "Starting F1 API containers"
+sudo docker-compose -f ergast-f1-api/docker-compose.yaml up -d
+
+echo "Removing old workshop-api1 and api2 containers"
+sudo docker container rm -f workshop-api1 workshop-api2
+echo "Starting API1 and API2"
+sudo docker run --restart unless-stopped --name workshop-api1 -d -p 81:80 -v $PWD/api1/:/app webdevops/php-nginx
+sudo docker run --restart unless-stopped --name workshop-api2 -d -p 82:80 -v $PWD/api2/:/app webdevops/php-nginx
+
+echo " test out: curl localhost:81"
+echo " and curl localhost:82"
