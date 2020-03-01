13fe36a3551ca2e8e8a0a88885ab2a596ee6d335
jessegoodier <jessegoodier@gmail.com>
2020-03-01 16:39:17 -0500

    add files

--- 0-install-required-dependencies.sh
+++ 0-install-required-dependencies.sh
@@ -1,3 +1,9 @@
 sudo apt -y install docker.io docker-compose python2.7 jq
+sudo systemctl enable docker
 sudo sh start_containers.sh
-
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+ansible-galaxy install nginxinc.nginx 
+ansible-galaxy install git+https://github.com/brianehlert/ansible-role-nginx-controller-generate-token.git
+ansible-galaxy install git+https://github.com/brianehlert/ansible-role-nginx-controller-agent.git
