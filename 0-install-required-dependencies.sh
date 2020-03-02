6964d7d41e6ed708d9405aa20e46c4e5e7b1d34d
jessegoodier <jessegoodier@gmail.com>
2020-03-02 14:59:56 -0500

    add files

--- 0-install-required-dependencies.sh
+++ 0-install-required-dependencies.sh
@@ -3,7 +3,7 @@ sudo systemctl enable docker
 sudo sh start_containers.sh
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt - y install ansible
+sudo apt -y install ansible
 ansible-galaxy install nginxinc.nginx 
 #ansible-galaxy collection install nginxinc.nginx_controller
 ansible-galaxy install nginxinc.nginx_controller_generate_token
